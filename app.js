ea09e665fb982d2bf2e546fbc3db5fe8af995eed
Jgorenburg <jacobgorenburg@gmail.com>
2024-09-17 14:28:33 -0400

    Add custom error and id padding

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ app.post('/receipts/process', (req, res) => {
     try {
         const points = calculatePoints(req.body)
         storage.push(points)
-        res.json({ "id": storage.length - 1 })
+        const id = (storage.length - 1).toString().padStart(6, 0)
+        res.json({ "id": id })
     } catch (error) {
         res.status(400).send("The receipt is invalid")
         throw error
@@ -32,8 +33,19 @@ app.listen(3000, () => {
 })
 
 
+class ReceiptError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "ReceiptError";
+    }
+}
+
 function calculatePoints(receipt) {
     // Points from the Retailer's name
+    if (receipt.retailer == "") {
+        throw new ReceiptError("Retailer must have a name")
+    }
+
     let namePoints = 0
     for (i = 0; i < receipt.retailer.length; i++) {
         code = receipt.retailer.charCodeAt(i)
@@ -44,7 +56,7 @@ function calculatePoints(receipt) {
         }
         // Only other allowed characters are '-', '&', and whitespace
         else if (code != 38 && code != 45 && !(/\s/.test(receipt.retailer[i]))) {
-            throw new Error("Malformed retailer name")
+            throw new ReceiptError("Malformed retailer name")
         }
     }
 
@@ -65,7 +77,7 @@ function calculatePoints(receipt) {
         }
     }
     else {
-        throw new Error("Invalid dollar amount")
+        throw new ReceiptError("Invalid dollar amount")
     }
 
 
@@ -74,18 +86,21 @@ function calculatePoints(receipt) {
     const descFormat = /^[\w\s\-]+$/
 
     if (items.length == 0) {
-        throw new Error("Receipt must contain at least one purchased item")
+        throw new ReceiptError("Receipt must contain at least one purchased item")
     }
 
     let itemPoints = Math.floor(items.length / 2) * 5
     for (i = 0; i < items.length; i++) {
         const item = items[i]
 
+        if (item.shortDescription == "") {
+            throw new ReceiptError("All items must have descriptions")
+        }
         if (!descFormat.test(item.shortDescription)) {
-            throw new Error(`${item.shortDescription} is not a valid item description`)
+            throw new ReceiptError(`${item.shortDescription} is not a valid item description`)
         }
         if (!priceFormat.test(item.price)) {
-            throw new Error(`The item ${item.shortDescription}'s price is not in the expected format`)
+            throw new ReceiptError(`The item ${item.shortDescription}'s price is not in the expected format`)
         }
 
         if (item.shortDescription.trim().length % 3 == 0) {
@@ -101,7 +116,7 @@ function calculatePoints(receipt) {
 
     const date = receipt.purchaseDate.match(dateFormat)
     if (!date) {
-        throw new Error("Invalid date")
+        throw new ReceiptError("Invalid date")
     }
     else if (date[1] % 2 == 1) {
         dateTimePoints = 6
@@ -109,15 +124,15 @@ function calculatePoints(receipt) {
 
     const time = receipt.purchaseTime.match(timeFormat)
     if (!time) {
-        throw new Error("Invalid time")
+        throw new ReceiptError("Invalid time")
     }
-    const hour = time[1]
-    if (hour == "15") {
-        dateTimePoints += 10
-    }
+
     // I'm treating the range as not inclusive, so if a purchase is at exactly 14:00, it
     // will award no points
-    else if (hour == "14" && time[2] != "00") {
+    const hour = time[1]
+    if (hour == "15" ||
+        (hour == "14" && time[2] != "00")
+    ) {
         dateTimePoints += 10
     }
 
